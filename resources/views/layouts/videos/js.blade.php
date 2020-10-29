f1280ffee47f6bb14a1a22793e3ee8bd1a7001c3
Qwinsie <53104056+Qwinsie@users.noreply.github.com>
2020-10-29 18:21:41 +0100

    css & js for scenes. (has to go to css and js folder)

---
+++ resources/views/layouts/videos/js.blade.php
@@ -0,0 +1,14 @@
+<script>
+    let video = document.getElementById("myVideo");
+    let btn = document.getElementById("myBtn");
+
+    function myFunction() {
+        if (video.paused) {
+            video.play();
+            btn.innerHTML = "Pause";
+        } else {
+            video.pause();
+            btn.innerHTML = "Play";
+        }
+    }
+</script>
