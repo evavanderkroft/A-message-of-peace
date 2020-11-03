847f70286e8ee98ce8c9c889e33ac079c55ba179
Qwinsie <53104056+Qwinsie@users.noreply.github.com>
2020-11-03 18:38:48 +0100

    city_entrance View with video

--- resources/views/scenes/city_entrance.blade.php
+++ resources/views/scenes/city_entrance.blade.php
@@ -4,23 +4,31 @@
     <main class="index">
         @include ('layouts.partials.header')
         <section class="container mx-auto px-4 pt-16">
-            <h1>Cit Entrance</h1>
+            <h1>City Entrance</h1>
         </section>
-        @include('layouts.videos.css')
+        <div class="image">
+            <div id="video-toggle" style="display: none">
+                @include('layouts.videos.css')
 
-        <video poster autoplay muted loop id="myVideo">
-            <source src="videos/1.mp4" type="video/mp4">
-            Your browser does not support HTML5 video.
-        </video>
+                <video id="video" poster autoplay loop>
+                    <source src="videos/1.mp4" type="video/mp4">
+                    Your browser does not support HTML5 video.
+                </video>
 
-        <div class="content">
-            <h1>Heading</h1>
-            <p>Lorem ipsum dolor sit amet, an his etiam torquatos. Tollit soleat phaedrum te duo, eum cu recteque expetendis neglegentur. Cu mentitum maiestatis persequeris pro, pri ponderum tractatos ei. Id qui nemore latine molestiae, ad mutat oblique delicatissimi pro.</p>
-            <button id="myBtn" onclick="myFunction()">Pause</button>
-            <button id="myBtn" onclick="myFunction()">Mute</button>
+                <div class="content">
+                    <h1>At the Market</h1><br>
+                    <button id="pauseBtn" class="myBtn" onclick="pauseVideo()">Play</button>
+                    <button id="muteBtn" class="myBtn" onclick="muteVideo()">Unmute</button>
+                    <button id="showVideoBtn" class="myBtn" onclick="quitVideo()">X</button>
+                </div>
+
+            </div>
+            <div id="button-toggle" style="display: block">
+                <button id="showVideoBtn" style="display: block" class="buttoncircle" onclick="showVideo()"></button>
+                <button id="backBtn" class="backbtn" onclick="goBack()">Back</button>
+            </div>
+            @include('layouts.videos.js')
         </div>
-
-        @include('layouts.videos.js')
     </main>
     {{--footer--}}
     @include ('layouts.partials.footer')
