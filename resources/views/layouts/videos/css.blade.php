c1868de70934f78007d7e0ccd2aaade9a2a94f08
Qwinsie <53104056+Qwinsie@users.noreply.github.com>
2020-11-03 18:39:47 +0100

    css.blade for city_entrance

--- resources/views/layouts/videos/css.blade.php
+++ resources/views/layouts/videos/css.blade.php
@@ -9,13 +9,27 @@
         font-size: 17px;
     }
 
-    #myVideo {
+    #video {
         position: fixed;
         right: 0;
         bottom: 0;
         min-width: 100%;
         min-height: 100%;
     }
+
+    .image {
+        background-image: url("img/city_entrance.jpg");
+        position: fixed;
+        right: 0;
+        bottom: 0;
+        min-width: 100%;
+        min-height: 100%;
+        background-color: #cccccc;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+
 
     .content {
         position: fixed;
@@ -26,7 +40,7 @@
         padding: 20px;
     }
 
-    #myBtn {
+    .myBtn {
         width: 200px;
         font-size: 18px;
         padding: 10px;
@@ -36,8 +50,50 @@
         cursor: pointer;
     }
 
-    #myBtn:hover {
+    .myBtn:hover {
         background: #ddd;
         color: black;
+    }
+
+    .buttoncircle {
+        background-color: #fc6e91;
+        border: 5px solid rgba(255,255,255,.75);
+        color: white;
+        position: absolute;
+        transition: .5s ease;
+        top:30%;
+        left:20%;
+        padding: 20px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        margin: 4px 2px;
+        border-radius: 50%;
+    }
+
+    .buttoncircle:hover {
+        border: 1px solid rgba(255,255,255,.75);
+    }
+
+    .backbtn {
+        background-color: #fff6f8;
+        border: 5px solid rgba(255,255,255,.75);
+        color: #000000;
+        position: absolute;
+        transition: .5s ease;
+        top:85%;
+        left:50%;
+        padding: 20px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        margin: 4px 2px;
+        border-radius: 50%;
+    }
+
+    .backbtn:hover {
+        border: 1px solid rgba(255,255,255,.75);
     }
 </style>
