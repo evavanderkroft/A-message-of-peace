ffe21b3735d2ff5dacf4afe4c54499837d6a908c
Qwinsie <53104056+Qwinsie@users.noreply.github.com>
2020-11-18 11:26:36 +0100

    JS voor popup.

---
+++ resources/js/popup.js
@@ -0,0 +1,12 @@
+
+let scene = document.getElementById('scene');
+
+let popup = document.getElementById("popup");
+
+    function showPopup() {
+        if (popup.style.display === "none") {
+            popup.style.display = "block";
+        } else {
+            popup.style.display = "none";
+        }
+    }
