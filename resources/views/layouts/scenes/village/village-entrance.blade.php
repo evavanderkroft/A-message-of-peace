77cd7ea40358d33e9c64d97e7d976aa0e62e9926
rikvermeulen <rik.vermeulen.1997@live.nl>
2020-11-19 22:44:38 +0100

    first test scene (work in progress)

--- resources/views/layouts/scenes/village/village-entrance.blade.php
+++ resources/views/layouts/scenes/village/village-entrance.blade.php
@@ -1,13 +1,27 @@
 @extends('layouts')
 
 @section('content')
-    <main class="index overflow-hidden  m-0 ">
+    <main class="index overflow-hidden  m-0">
         @include ('layouts.partials.header')
         <section class="">
             <div class="scene pointer-events-auto" id="scene">
-                <li class="layer layer-1 z-10" data-depth="0.90" data-depth-y="0.20" style="">
-                    <div class="background-layer-1">
+                <li class="layer layer-1-village z-10" data-depth="0.70" data-depth-y="0.10" style="">
+                    <div class="background-village-1">
                     </div>
+
+
+                </li>
+                <li class="layer layer-2-village z-20" data-depth="0.60" style="">
+                    <div class="background-village-2">
+
+                    </div>
+                </li>
+                <li class="layer layer-3-village z-30" data-depth="0.50"  style="">
+                    <div class="background-village-3">
+
+                    </div>
+                </li>
+                <li class="layer layer-nav z-40" data-depth="0.60" style="">
                     <div class="button-top">
                         <a class="z-50 text-gray-500 absolute arrow-top" href="{{ route('village-waterwell') }}">
                         </a>
@@ -27,17 +41,6 @@
                         <a class="z-50 text-gray-500 absolute arrow-right" href="{{ route('village-school') }}">
                         </a>
                         <span>Go to the School</span>
-                    </div>
-                </li>
-                <li class="layer layer-2 z-0" data-depth="0.10" style="">
-                    <img src="{{ asset('/images/sky.jpg') }}" alt="">
-                    <div class="background-layer-2">
-
-                    </div>
-                </li>
-                <li class="layer layer-3 z-20" data-depth="0.60" data-depth-y="0.30" style="">
-                    <div class="background-layer-3">
-
                     </div>
                 </li>
             </div>
