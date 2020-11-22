ca287dc70fe23144045d161837de2471758f2d35
rikvermeulen <rik.vermeulen.1997@live.nl>
2020-11-22 14:53:13 +0100

    first,second&third edited (work in progress)

--- resources/views/layouts/scenes/city/city-entrance.blade.php
+++ resources/views/layouts/scenes/city/city-entrance.blade.php
@@ -5,30 +5,6 @@
         @include ('layouts.partials.header')
         <section class="">
             <div class="scene pointer-events-auto" id="scene">
-                <li class="layer scene1-layer-1 z-10" data-depth="0.90" data-depth-y="0.20" style="">
-                    <div class="scene1-background-layer-1">
-                    </div>
-                    <div class="arrow_container" style="top:50%; left:45%;" >
-                        <svg height="120" width="120" style="transform:rotate(180deg)">
-                            <a href="{{ route('main-road') }}" >
-                                <polygon class="arrow" points=" 45,5 85,95 45,75 5,95"/>
-                            </a>
-                        </svg>
-                        <div class="arrow_text">
-                            <p>Go back to the Main Road</p>
-                        </div>
-                    </div>
-                </li>
-                <li class="layer scene1-layer-2 z-0" data-depth="0.10" style="">
-                    <div class="scene1-background-layer-2">
-
-                    </div>
-                </li>
-                <li class="layer scene1-layer-3 z-20" data-depth="0.60" data-depth-y="0.30" style="">
-                    <div class="scene1-background-layer-3">
-
-                    </div>
-                </li>
                 <li class="layer layer-1-city z-0" data-depth="0.90" data-depth-y="0.20" style="">
                     <div class="background-city-1" id="sky">
                     </div>
@@ -39,6 +15,18 @@
                 </li>
                 <li class="layer layer-3-city z-20"  data-depth="0.30" >
                     <div class="background-city-3">
+                    </div>
+                </li>
+                <li class="layer layer-nav z-40" data-depth="0.60" data-depth-y="0.10">
+                    <div class="arrow_container" style="bottom:20%; left:45%;" >
+                        <svg viewBox="0 0 120 120" height="60px" width="60px" style="transform:rotate(180deg)">
+                            <a href="{{ route('main-road') }}" >
+                                <polygon class="arrow" points=" 45,5 85,95 45,75 5,95"/>
+                            </a>
+                        </svg>
+                        <div class="arrow_text">
+                            <p>Go back to the Main Road</p>
+                        </div>
                     </div>
                 </li>
             </div>
