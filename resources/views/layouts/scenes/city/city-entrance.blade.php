89569aff5a1acd56d5e9cf28444b62a866499006
rikvermeulen <rik.vermeulen.1997@live.nl>
2020-11-22 14:48:40 +0100

    first,second&third edited (work in progress)

--- resources/views/layouts/scenes/city/city-entrance.blade.php
+++ resources/views/layouts/scenes/city/city-entrance.blade.php
@@ -8,36 +8,6 @@
                 <li class="layer scene1-layer-1 z-10" data-depth="0.90" data-depth-y="0.20" style="">
                     <div class="scene1-background-layer-1">
                     </div>
-{{--                    <div class="arrow_container" style="top:10%; left:46%;" >--}}
-{{--                        <svg height="120" width="120" style="transform:rotate(0deg)">--}}
-{{--                            <a href="{{ route('city-slum') }}" >--}}
-{{--                                <polygon class="arrow" points=" 45,5 85,95 45,75 5,95"/>--}}
-{{--                            </a>--}}
-{{--                        </svg>--}}
-{{--                        <div class="arrow_text">--}}
-{{--                            <p>Go to the Slum</p>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                    <div class="arrow_container" style="top:35%; left:10%;" >--}}
-{{--                        <svg height="120" width="120" style="transform:rotate(280deg)">--}}
-{{--                            <a href="{{ route('city-rich') }}" >--}}
-{{--                                <polygon class="arrow" points=" 45,5 85,95 45,75 5,95"/>--}}
-{{--                            </a>--}}
-{{--                        </svg>--}}
-{{--                        <div class="arrow_text">--}}
-{{--                            <p>Go to the Rich Place</p>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                    <div class="arrow_container" style="top:35%; right:10%;" >--}}
-{{--                        <svg height="120" width="120" style="transform:rotate(80deg)">--}}
-{{--                            <a href="{{ route('city-school') }}" >--}}
-{{--                                <polygon class="arrow" points=" 45,5 85,95 45,75 5,95"/>--}}
-{{--                            </a>--}}
-{{--                        </svg>--}}
-{{--                        <div class="arrow_text">--}}
-{{--                            <p>Go to the School</p>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
                     <div class="arrow_container" style="top:50%; left:45%;" >
                         <svg height="120" width="120" style="transform:rotate(180deg)">
                             <a href="{{ route('main-road') }}" >
@@ -50,7 +20,6 @@
                     </div>
                 </li>
                 <li class="layer scene1-layer-2 z-0" data-depth="0.10" style="">
-                    <img src="{{ asset('/images/sky.jpg') }}" alt="">
                     <div class="scene1-background-layer-2">
 
                     </div>
@@ -58,6 +27,18 @@
                 <li class="layer scene1-layer-3 z-20" data-depth="0.60" data-depth-y="0.30" style="">
                     <div class="scene1-background-layer-3">
 
+                    </div>
+                </li>
+                <li class="layer layer-1-city z-0" data-depth="0.90" data-depth-y="0.20" style="">
+                    <div class="background-city-1" id="sky">
+                    </div>
+                </li>
+                <li class="layer layer-2-city z-10" data-depth="0.40" style="">
+                    <div class="background-city-2">
+                    </div>
+                </li>
+                <li class="layer layer-3-city z-20"  data-depth="0.30" >
+                    <div class="background-city-3">
                     </div>
                 </li>
             </div>
