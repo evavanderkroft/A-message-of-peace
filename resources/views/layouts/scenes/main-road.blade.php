b68862dfbce603bdea66ee7e210b957a298c7555
0994516 <58770105+0994516@users.noreply.github.com>
2020-12-01 12:31:15 +0100

    arrow alter.3

--- resources/views/layouts/scenes/main-road.blade.php
+++ resources/views/layouts/scenes/main-road.blade.php
@@ -21,8 +21,8 @@
                     <div class="arrow_container" style="top:50vh; left:15%;" >
                         <svg viewBox="0 0 120 120" height="70px" width="70px" class="test filter">
                             <a href="{{ route('city-entrance') }}" >
-                                <polygon class="arrow" points=" 45,5 85,95 45,75 5,95"
-                                         style="transform: rotate(280deg); transform-origin: 50% 50%;"/>
+                                <polygon class="arrow" points="55,15 95,105 55,85 15,105"
+                                         style="transform: rotate(280deg); transform-origin: center; margin:10px;"/>
                             </a>
                         </svg>
                         <div class="arrow_text">
@@ -32,7 +32,7 @@
                     <div class="arrow_container" style="top:50vh; right:15%;" >
                         <svg viewBox="0 0 120 120" height="70px" width="70px" class="test filter" >
                             <a href="{{ route('village-entrance') }}">
-                                <polygon class="arrow" points=" 45,5 85,95 45,75 5,95"
+                                <polygon class="arrow" points="55,15 95,105 55,85 15,105"
                                          style="transform: rotate(80deg); transform-origin: 50% 50%;"/>
                             </a>
                         </svg>
