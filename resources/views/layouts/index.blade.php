bfe208c6747d734891576bda8c6d529ab1e237ed
Qwinsie <qvdsluis@gmail.com>
2020-12-08 14:12:43 +0100

    Added a link to Meet The Soldier.

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -12,7 +12,7 @@
                 <h1 class="text-title">a message of peace</h1>
                 <h2>A Message of Peace is an interactive experience demonstrating the social inequalities in Uganda.
                     <br>
-                    A Message of Peace is a sequel to the humanitarian documentary Meet the Soldier.
+                    A Message of Peace is a sequel to the humanitarian documentary <a class="hover:text-blue-400 underline" href="https://www.meetthesoldier.com">Meet the Soldier</a>.
                     <br>
                     The purpose of A Message of Peace is both to raise awareness and to educate those who interact with the experience.
                 </h2>
