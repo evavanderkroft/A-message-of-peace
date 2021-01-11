44962b0c6d9b7e4c75d011807e92dbf126d60519
0994516 <58770105+0994516@users.noreply.github.com>
2021-01-11 13:04:31 +0100

    putting buttons in the correct list City-entrance

--- resources/views/layouts/scenes/city/city-entrance.blade.php
+++ resources/views/layouts/scenes/city/city-entrance.blade.php
@@ -19,56 +19,58 @@
                 <li class="layer layer-4-mainroad z-40" data-depth="0"style="">
                     @include ('layouts.partials.header')
                 </li>
+                <li class="layer-content z-40"  data-depth="0">
+                    <div class="box-title z-50" style="bottom: 100vh">
+                        <p class="subtext">You are at the:</p>
+                        <h1 class="title">city</h1>
+                    </div>
+                    <div class="arrow_container" style="bottom:20vh; right:10vw;" >
+                        <svg viewBox="0 0 120 120" height="70px" width="70px" class="test filter" >
+                            <a href="{{ route('city-slum') }}" >
+                                <polygon class="arrow" points="55,15 95,105 55,85 15,105"
+                                         style="transform: rotate(80deg); transform-origin: 50% 50%;"/>
+                            </a>
+                        </svg>
+                        <div class="arrow_text">
+                            <p>Go to the Slum</p>
+                        </div>
+                    </div>
+                    <div class="arrow_container" style="bottom:20vh; left:10vw;" >
+                        <svg viewBox="0 0 120 120" height="70px" width="70px" class="test filter" >
+                            <a href="{{ route('city-rich') }}" >
+                                <polygon class="arrow" points="55,15 95,105 55,85 15,105"
+                                         style="transform: rotate(280deg); transform-origin: 50% 50%;"/>
+                            </a>
+                        </svg>
+                        <div class="arrow_text">
+                            <p>Go to the Rich</p>
+                        </div>
+                    </div>
+                    <div class="arrow_container" style="bottom:20vh; right:25vw;" >
+                        <svg viewBox="0 0 120 120" height="70px" width="70px" class="test filter" >
+                            <a href="{{ route('city-school') }}" >
+                                <polygon class="arrow" points="55,15 95,105 55,85 15,105"
+                                         style="transform: rotate(0deg); transform-origin: 50% 50%;"/>
+                            </a>
+                        </svg>
+                        <div class="arrow_text">
+                            <p>Go to the School</p>
+                        </div>
+                    </div>
+                    <div class="arrow_container" style="bottom:5%; left:45%;" >
+                        <svg viewBox="0 0 120 120" height="70px" width="70px" class="test filter" >
+                            <a href="{{ route('main-road') }}" >
+                                <polygon class="arrow" points="55,15 95,105 55,85 15,105"
+                                         style="transform: rotate(180deg); transform-origin: 50% 50%;"/>
+                            </a>
+                        </svg>
+                        <div class="arrow_text">
+                            <p>Go back to the Main Road</p>
+                        </div>
+                    </div>
+                </li>
             </div>
         </section>
-        <div class="box-title z-50" style="bottom: 100vh">
-            <p class="subtext">You are at the:</p>
-            <h1 class="title">city</h1>
-        </div>
-        <div class="arrow_container" style="bottom:20vh; right:10vw;" >
-            <svg viewBox="0 0 120 120" height="70px" width="70px" class="test filter" >
-                <a href="{{ route('city-slum') }}" >
-                    <polygon class="arrow" points="55,15 95,105 55,85 15,105"
-                             style="transform: rotate(80deg); transform-origin: 50% 50%;"/>
-                </a>
-            </svg>
-            <div class="arrow_text">
-                <p>Go to the Slum</p>
-            </div>
-        </div>
-        <div class="arrow_container" style="bottom:20vh; left:10vw;" >
-            <svg viewBox="0 0 120 120" height="70px" width="70px" class="test filter" >
-                <a href="{{ route('city-rich') }}" >
-                    <polygon class="arrow" points="55,15 95,105 55,85 15,105"
-                             style="transform: rotate(280deg); transform-origin: 50% 50%;"/>
-                </a>
-            </svg>
-            <div class="arrow_text">
-                <p>Go to the Rich</p>
-            </div>
-        </div>
-        <div class="arrow_container" style="bottom:20vh; right:25vw;" >
-            <svg viewBox="0 0 120 120" height="70px" width="70px" class="test filter" >
-                <a href="{{ route('city-school') }}" >
-                    <polygon class="arrow" points="55,15 95,105 55,85 15,105"
-                             style="transform: rotate(0deg); transform-origin: 50% 50%;"/>
-                </a>
-            </svg>
-            <div class="arrow_text">
-                <p>Go to the School</p>
-            </div>
-        </div>
-        <div class="arrow_container" style="bottom:5%; left:45%;" >
-            <svg viewBox="0 0 120 120" height="70px" width="70px" class="test filter" >
-                <a href="{{ route('main-road') }}" >
-                    <polygon class="arrow" points="55,15 95,105 55,85 15,105"
-                             style="transform: rotate(180deg); transform-origin: 50% 50%;"/>
-                </a>
-            </svg>
-            <div class="arrow_text">
-                <p>Go back to the Main Road</p>
-            </div>
-        </div>
     </main>
     {{--footer--}}
     @include ('layouts.partials.footer')
